09172820e16f54a46ef6b5651bfa44fbf39f7c3e
Victor Moraes <vdmoraes94@gmail.com>
2017-01-19 21:03:44 -0200

    Creates players and country models

--- datazeiro/jogos/views.py
+++ datazeiro/jogos/views.py
@@ -4,4 +4,3 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("This is the future jogos page.")
 
-# Create your views here.
--- datazeiro/players/models.py
+++ datazeiro/players/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+	name = models.CharField(max_length=100)
+	abbreviation = models.CharField(max_length=3)
+
+class Player(models.Model):
+	first_name = models.CharField(max_length=200)
+	second_name = models.CharField(max_length=200)
+	known_as = models.CharField(max_length=200)
+	age = models.IntegerField(default=0)
+	nationality = models.ForeignKey(Country, on_delete=models.CASCADE)
+	goals = models.IntegerField(default=0)
+	assists = models.IntegerField(default=0)
+	height = models.IntegerField(default=0)
+	weight = models.IntegerField(default=0)
